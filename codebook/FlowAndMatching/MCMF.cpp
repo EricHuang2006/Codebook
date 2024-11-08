489fbdaf81c3f6f8c2799af32940a4782f4b622a
phoenix0423 <ericabcd423@gmail.com>
2024-11-08 17:11:48 +0800

    add flow

---
+++ codebook/FlowAndMatching/MCMF.cpp
@@ -0,0 +1,74 @@
+struct MCMF{
+    using T = int;
+    struct edge{
+        int to;
+        T cap, cost;
+        edge(){}
+        edge(int _to, T _cap, T _cost) : to(_to), cap(_cap), cost(_cost){}
+    };
+    vector<edge> e;
+    vector<vector<int>> adj;
+    vector<int> iter, inq;
+    vector<T> dist;
+    int n, s, t;
+    void init(int _n, int _s, int _t){
+        n = _n, s = _s, t = _t;
+        adj = vector<vector<int>>(n);
+        iter = vector<int>(n);
+        dist = vector<T>(n);
+        inq = vector<int>(n);
+        e.clear();
+    }
+    void add_edge(int from, int to, T cap, T cost = 0){
+        adj[from].pb(e.size()), adj[to].pb(e.size() + 1);
+        e.pb(edge(to, cap, cost)), e.pb(edge(from, 0, -cost));
+    }
+    bool spfa(){
+        fill(dist.begin(), dist.end(), INF);
+        queue<int> q;
+        q.push(s);
+        dist[s] = 0, inq[s] = 1;
+        while(!q.empty()){
+            int pos = q.front(); q.pop();
+            inq[pos] = 0;
+            for(auto id : adj[pos]){
+                auto [to, cap, cost] = e[id];
+                if(cap && dist[to] > dist[pos] + cost){
+                    dist[to] = dist[pos] + cost;
+                    if(!inq[to]) q.push(to), inq[to] = 1;
+                }
+            }
+        }
+        return dist[t] != INF;
+    }
+    T dfs(int pos, T flow){
+        if(pos == t) return flow;
+        inq[pos] = 1;
+        for(int &i = iter[pos]; i < adj[pos].size(); i++){
+            auto [to, cap, cost] = e[adj[pos][i]];
+            if(!inq[to] && dist[to] == dist[pos] + cost && cap){
+                T tmp = dfs(to, min(flow, cap));
+                if(tmp){
+                    inq[pos] = 0;
+                    e[adj[pos][i]].cap -= tmp;
+                    e[adj[pos][i] ^ 1].cap += tmp;
+                    return tmp; 
+                }
+            }
+        }
+        inq[pos] = 0;
+        return 0;
+    }
+    pair<T, T> mcmf(){
+        T flow = 0, cost = 0;
+        while(true){
+            if(!spfa()) break;
+            fill(iter.begin(), iter.end(), 0);
+            T tmp;
+            while((tmp = dfs(s, INF)) > 0){
+                flow += tmp, cost += tmp * dist[t];
+            }
+        }
+        return {flow, cost};
+    }
+} flow;
