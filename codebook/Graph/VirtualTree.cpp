a3a429b4225610bed71b97bdbb185c9630547af5
phoenix0423 <ericabcd423@gmail.com>
2024-11-12 23:03:06 +0800

    update

---
+++ codebook/Graph/VirtualTree.cpp
@@ -0,0 +1,18 @@
+// need lca, in, out
+vector<pll> virtual_tree(vector<int> &v) {
+  auto cmp = [&](int x, int y) {return in[x] < in[y];};
+  sort(all(v), cmp);
+  int sz = (int)v.size();
+  for (int i = 0; i + 1 < sz; ++i)
+    v.pb(lca(v[i], v[i + 1]));
+  sort(all(v), cmp);
+  v.resize(unique(all(v)) - v.begin());
+  vector <int> stk(1, v[0]);
+  vector <pll> res;
+  for (int i = 1; i < (int)v.size(); ++i) {
+    int x = v[i];
+    while (out[stk.back()] < out[x]) stk.pop_back();
+    res.emplace_back(stk.back(), x), stk.pb(x);
+  }
+  return res;
+}
